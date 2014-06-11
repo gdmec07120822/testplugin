1cfb91d0dcaad5c531d159f6bb63d09c47301d44
你的姓名 <>
2014-06-11 21:28:36 +0800

    “提交注释”

---
+++ platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\testplugin\platforms\android\bin\HelloWorld-debug-unaligned.apk : \
+C:\testplugin\platforms\android\bin\HelloWorld.ap_ \
+C:\testplugin\platforms\android\bin\classes.dex \
+C:\testplugin\platforms\android\CordovaLib\bin\classes.jar \
---
+++ platforms/android/bin/classes.dex.d
@@ -0,0 +1,9 @@
+C:\testplugin\platforms\android\bin\classes.dex : \
+C:\testplugin\platforms\android\bin\classes\com\gdmec\testplugin\BuildConfig.class \
+C:\testplugin\platforms\android\bin\classes\com\gdmec\testplugin\HelloWorld.class \
+C:\testplugin\platforms\android\bin\classes\com\gdmec\testplugin\R$attr.class \
+C:\testplugin\platforms\android\bin\classes\com\gdmec\testplugin\R$drawable.class \
+C:\testplugin\platforms\android\bin\classes\com\gdmec\testplugin\R$string.class \
+C:\testplugin\platforms\android\bin\classes\com\gdmec\testplugin\R$xml.class \
+C:\testplugin\platforms\android\bin\classes\com\gdmec\testplugin\R.class \
+C:\testplugin\platforms\android\bin\dexedLibs\classes-e52c6e8428f22ec426fbbf812b7246f1.jar \
